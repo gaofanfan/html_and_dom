a6439f7e3ca4e7bae837713dadf8204086d0d1a9
gaofanfan <1125774809@qq.com>
2014-12-17 14:29:06 +0800

    rename

--- main.js
+++ main.js
@@ -1,104 +1,104 @@
-function scores() {
-  var grade = 0 ;
-  grade += filling() ;
-  grade += choice() ;
-  grade += multipleChoice() ;
-  grade += judge() ;
-  grade += question() ;
-  document.getElementById('count').value = grade;
+function printScore() {
+  var score = 0 ;
+  score += filling() ;
+  score += choice() ;
+  score += multipleChoice() ;
+  score += judge() ;
+  score += question() ;
+  document.getElementById('count').value = score;
 
 }
 
 function filling() {
-  var grade = 0 ;
-  var answer1_1 = document.getElementById('UML');
-  var answer2_1 = document.getElementById('one');
-  var answer2_2 = document.getElementById('two');
-  var answer2_3 = document.getElementById('three');
+  var score = 0 ;
+  var answer1_1 = document.getElementById('1_1');
+  var answer2_1 = document.getElementById('2_1');
+  var answer2_2 = document.getElementById('2_2');
+  var answer2_3 = document.getElementById('2_3');
   if (answer1_1.value === '统一建模语言') {
-    grade += 10 ;
+    score += 10 ;
   }
   if (answer2_1.value === '封装性') {
-    grade += 5 ;
+    score += 5 ;
   }
   if (answer2_2.value === '继承性') {
-    grade += 5 ;
+    score += 5 ;
   }
   if (answer2_3.value === '多态性') {
-    grade += 5 ;
+    score += 5 ;
   }
-  return grade ;
+  return score ;
 }
 function choice() {
-  var grade = 0 ;
-  var select1 = document.getElementsByName('select1');
-  for (var i = 0;i < select1.length; i++) {
-    if (select1[i].checked && select1[i].value === "B") {
-      grade += 10 ;
+  var score = 0 ;
+  var answer2_1_1 = document.getElementsByName('2_1_1');
+  for (var i = 0;i < answer2_1_1.length; i++) {
+    if (answer2_1_1[i].checked && answer2_1_1[i].value === "B") {
+      score += 10 ;
     }
   }
 
-  var select2 = document.getElementsByName('select2');
-  for (var j = 0 ;j < select2.length; j++) {
-    if (select2[j].checked && select2[j].value === "A") {
-      grade += 10 ;
+  var answer2_1_2 = document.getElementsByName('2_2_1');
+  for (var j = 0 ;j <  answer2_1_2 .length; j++) {
+    if ( answer2_1_2[j].checked &&  answer2_1_2 [j].value === "A") {
+      score += 10 ;
     }
   }
-  return grade ;
+  return score ;
 }
 
 function multipleChoice() {
-  var grade = 0 ;
+  var score = 0 ;
   var array1 = [];
   var array2 = [];
-  var select1 = document.getElementsByName('moreSelect1');
-  var select2 = document.getElementsByName('moreSelect2');
-  for (var i = 0; i < select1.length; i++) {
-    if (select1[i].checked === true) {
-      array1.push(select1[i].value);
+  var answer3_1 = document.getElementsByName('3_1');
+  var answer3_2 = document.getElementsByName('3_2');
+  for (var i = 0; i < answer3_1.length; i++) {
+    if (answer3_1[i].checked === true) {
+      array1.push(answer3_1[i].value);
     }
   }
 
   if (array1.toString() === "A,B,D") {
-      grade += 10 ;
+     score += 10 ;
   }
 
-  for (var j = 0; j < select2.length; j++) {
-    if (select2[j].checked === true) {
-      array2.push(select2[j].value);
+  for (var j = 0; j < answer3_2.length; j++) {
+    if (answer3_2[j].checked === true) {
+      array2.push(answer3_2[j].value);
     }
   }
   if (array2.toString() === "A,B,C") {
-    grade += 10 ;
+    score += 10 ;
   }
 
- return grade;
+ return score;
 }
 
 function judge() {
-  var grade = 0;
-  var judge1 = document.getElementsByName("judge1");
-  var judge2 = document.getElementsByName("judge2");
+  var score = 0;
+  var judge1 = document.getElementsByName("4_1");
+  var judge2 = document.getElementsByName("4_2");
   for (var i = 0; i < judge1.length;i++) {
     if (judge1[i].checked && judge1[i].value === 'N') {
-      grade += 10;
+      score += 10;
     }
   }
   for (var j = 0;j < judge2.length; j++) {
     if (judge2[j].checked && judge2[j].value === 'Y') {
-      grade += 10;
+      score += 10;
     }
   }
-  return grade;
+  return score;
 }
 
 function question() {
-  var grade = 0;
+  var score = 0;
   var string = "模型是对现实世界的简化和抽象，模型是对所研究的系统过程、事物或概念的一种表达形式。可以是物理实体；可以是某种图形或者是一种数学表达式。";
-  var questions = document.getElementById('questions');
+  var questions = document.getElementById('5_1');
 
   if (questions.value === string) {
-    grade += 10;
+    score += 10;
   }
-  return grade;
+  return score;
 }
